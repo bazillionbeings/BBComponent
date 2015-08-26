5b90ed743131f08ac95b027c7fdb5839d1201d7b
Ara Yeressian <ayeressian2@gmail.com>
2015-08-26 14:11:43 +0400

    index

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var constValues = require('./constValues.json');
+var Component = require('./Component.js');
+var ComponentResult = require('./ComponentResult.js');
+
+module.exports = {
+  constValues: constValues,
+  Component: Component,
+  ComponentResult: ComponentResult
+};
