0ec2aa499dec976d2e537b19aa415710a770186c
Ara Yeressian <ayeressian2@gmail.com>
2015-10-01 16:06:18 +0400

    changed to new design

--- Component.js
+++ Component.js
@@ -1,20 +1,26 @@
 'use strict';
 
-function Component(controllerCallBacks, dataStore, types) {
-  this._inputs = this._getInput(dataStore, types);
-  this._controllerCallBacks = controllerCallBacks;
-}
+class Component {
+  constructor(controllerCallBacks, dataStore, types) {
+    this._inputs = this._getInput(dataStore, types);
+    this._controllerCallBacks = controllerCallBacks;
+  }
 
-Component.prototype._getInput = function(dataStore, types) {
-  var result = {};
-  dataStore.forEach(function(data) {
-    types.forEach(function(type) {
-      if (type === data.type) {
-        result[type] = data.data;
-      }
+  _getInput(dataStore, types) {
+    var result = {};
+    dataStore.forEach(function (data) {
+      types.forEach(function (type) {
+        if (type === data.type) {
+          result[type] = data.data;
+        }
+      });
     });
-  });
-  return result;
-};
+    return result;
+  }
+
+  setDataStore(dataStore) {
+    this._dataStore = datastore;
+  }
+}
 
 module.exports = Component;
